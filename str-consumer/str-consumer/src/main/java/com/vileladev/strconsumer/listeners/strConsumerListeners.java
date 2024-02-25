226961fd7ea498d83e08ebecaa1c077dccc2df51
vileladev <rhcp_fabio@hotmail.com>
2024-02-25 13:34:53 -0300

    partition test

---
+++ str-consumer/str-consumer/src/main/java/com/vileladev/strconsumer/listeners/strConsumerListeners.java
@@ -0,0 +1,34 @@
+package com.vileladev.strconsumer.listeners;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
+import org.springframework.stereotype.Component;
+
+@Component
+@Log4j2
+public class strConsumerListeners {
+
+    @KafkaListener(groupId = "group-1", topicPartitions = {
+            @TopicPartition(topic = "str-topic", partitions = {"0"})
+             }
+            ,containerFactory = "strContainerFactory")
+    public void create(String message){
+        log.info("CREATE ::: Receive message{}", message);
+    }
+
+    @KafkaListener(groupId = "group-1", topicPartitions = {
+            @TopicPartition(topic = "str-topic", partitions = {"1"})
+    }
+            ,containerFactory = "strContainerFactory")
+    public void log(String message){
+        log.info("LOG ::: Receive message{}", message);
+    }
+
+    @KafkaListener(groupId = "group-2", topics = "str-topic"
+            ,containerFactory = "strContainerFactory")
+    public void hist(String message){
+        log.info("HIST ::: Receive message{}", message);
+    }
+
+}
